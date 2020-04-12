166b59a54ac5c24fb6ef40aacf6982c0f365e3e4
Vladislav Shulyak <vladislav.dll@gmail.com>
2020-04-12 23:57:25 +0300

    feat: implement training mode and fix some ui bags

--- src/index.js
+++ src/index.js
@@ -1,15 +1,406 @@
+// const { categories } = require('./cards.js');
+// const { cards } = require('./cards.js');
+const categories = ['Actions Part 2', 'Actions Part 3', 'Animals Part 2', 'Animals Part 3', 'Clothes', 'Emotion'];
+const cards = [
+    [
+        {
+            word: 'cry',
+            translation: 'плакать',
+            image: './src/img/cry.jpg',
+            audioSrc: './src/audio/cry.mp3',
+        },
+        {
+            word: 'dance',
+            translation: 'танцевать',
+            image: './src/img/dance.jpg',
+            audioSrc: './src/audio/dance.mp3',
+        },
+        {
+            word: 'dive',
+            translation: 'нырять',
+            image: './src/img/dive.jpg',
+            audioSrc: './src/audio/dive.mp3',
+        },
+        {
+            word: 'draw',
+            translation: 'рисовать',
+            image: './src/img/draw.jpg',
+            audioSrc: './src/audio/draw.mp3',
+        },
+        {
+            word: 'fish',
+            translation: 'ловить рыбу',
+            image: './src/img/fish.jpg',
+            audioSrc: './src/audio/fish.mp3',
+        },
+        {
+            word: 'fly',
+            translation: 'летать',
+            image: './src/img/fly.jpg',
+            audioSrc: './src/audio/fly.mp3',
+        },
+        {
+            word: 'hug',
+            translation: 'обнимать',
+            image: './src/img/hug.jpg',
+            audioSrc: './src/audio/hug.mp3',
+        },
+        {
+            word: 'jump',
+            translation: 'прыгать',
+            image: './src/img/jump.jpg',
+            audioSrc: './src/audio/jump.mp3',
+        },
+    ],
+    [
+        {
+            word: 'open',
+            translation: 'открывать',
+            image: './src/img/open.jpg',
+            audioSrc: './src/audio/open.mp3',
+        },
+        {
+            word: 'play',
+            translation: 'играть',
+            image: './src/img/play.jpg',
+            audioSrc: './src/audio/play.mp3',
+        },
+        {
+            word: 'point',
+            translation: 'указывать',
+            image: './src/img/point.jpg',
+            audioSrc: './src/audio/point.mp3',
+        },
+        {
+            word: 'ride',
+            translation: 'ездить',
+            image: './src/img/ride.jpg',
+            audioSrc: './src/audio/ride.mp3',
+        },
+        {
+            word: 'run',
+            translation: 'бегать',
+            image: './src/img/run.jpg',
+            audioSrc: './src/audio/run.mp3',
+        },
+        {
+            word: 'sing',
+            translation: 'петь',
+            image: './src/img/sing.jpg',
+            audioSrc: './src/audio/sing.mp3',
+        },
+        {
+            word: 'skip',
+            translation: 'пропускать, прыгать',
+            image: './src/img/skip.jpg',
+            audioSrc: './src/audio/skip.mp3',
+        },
+        {
+            word: 'swim',
+            translation: 'плавать',
+            image: './src/img/swim.jpg',
+            audioSrc: './src/audio/swim.mp3',
+        },
+    ],
+    [
+        {
+            word: 'cat',
+            translation: 'кот',
+            image: './src/img/cat.jpg',
+            audioSrc: './src/audio/cat.mp3',
+        },
+        {
+            word: 'chick',
+            translation: 'цыплёнок',
+            image: './src/img/chick.jpg',
+            audioSrc: './src/audio/chick.mp3',
+        },
+        {
+            word: 'chicken',
+            translation: 'курица',
+            image: './src/img/chicken.jpg',
+            audioSrc: './src/audio/chicken.mp3',
+        },
+        {
+            word: 'dog',
+            translation: 'собака',
+            image: './src/img/dog.jpg',
+            audioSrc: './src/audio/dog.mp3',
+        },
+        {
+            word: 'horse',
+            translation: 'лошадь',
+            image: './src/img/horse.jpg',
+            audioSrc: './src/audio/horse.mp3',
+        },
+        {
+            word: 'pig',
+            translation: 'свинья',
+            image: './src/img/pig.jpg',
+            audioSrc: './src/audio/pig.mp3',
+        },
+        {
+            word: 'rabbit',
+            translation: 'кролик',
+            image: './src/img/rabbit.jpg',
+            audioSrc: './src/audio/rabbit.mp3',
+        },
+        {
+            word: 'sheep',
+            translation: 'овца',
+            image: './src/img/sheep.jpg',
+            audioSrc: './src/audio/sheep.mp3',
+        },
+    ],
+    [
+        {
+            word: 'bird',
+            translation: 'птица',
+            image: './src/img/bird.jpg',
+            audioSrc: './src/audio/bird.mp3',
+        },
+        {
+            word: 'fish',
+            translation: 'рыба',
+            image: './src/img/fish1.jpg',
+            audioSrc: './src/audio/fish.mp3',
+        },
+        {
+            word: 'frog',
+            translation: 'жаба',
+            image: './src/img/frog.jpg',
+            audioSrc: './src/audio/frog.mp3',
+        },
+        {
+            word: 'giraffe',
+            translation: 'жирафа',
+            image: './src/img/giraffe.jpg',
+            audioSrc: './src/audio/giraffe.mp3',
+        },
+        {
+            word: 'lion',
+            translation: 'лев',
+            image: './src/img/lion.jpg',
+            audioSrc: './src/audio/lion.mp3',
+        },
+        {
+            word: 'mouse',
+            translation: 'мышь',
+            image: './src/img/mouse.jpg',
+            audioSrc: './src/audio/mouse.mp3',
+        },
+        {
+            word: 'turtle',
+            translation: 'черепаха',
+            image: './src/img/turtle.jpg',
+            audioSrc: './src/audio/turtle.mp3',
+        },
+        {
+            word: 'dolphin',
+            translation: 'дельфин',
+            image: './src/img/dolphin.jpg',
+            audioSrc: './src/audio/dolphin.mp3',
+        },
+    ],
+    [
+        {
+            word: 'skirt',
+            translation: 'юбка',
+            image: './src/img/skirt.jpg',
+            audioSrc: './src/audio/skirt.mp3',
+        },
+        {
+            word: 'pants',
+            translation: 'брюки',
+            image: './src/img/pants.jpg',
+            audioSrc: './src/audio/pants.mp3',
+        },
+        {
+            word: 'blouse',
+            translation: 'блузка',
+            image: './src/img/blouse.jpg',
+            audioSrc: './src/audio/blouse.mp3',
+        },
+        {
+            word: 'dress',
+            translation: 'платье',
+            image: './src/img/dress.jpg',
+            audioSrc: './src/audio/dress.mp3',
+        },
+        {
+            word: 'boot',
+            translation: 'ботинок',
+            image: './src/img/boot.jpg',
+            audioSrc: './src/audio/boot.mp3',
+        },
+        {
+            word: 'shirt',
+            translation: 'рубашка',
+            image: './src/img/shirt.jpg',
+            audioSrc: './src/audio/shirt.mp3',
+        },
+        {
+            word: 'coat',
+            translation: 'пальто',
+            image: './src/img/coat.jpg',
+            audioSrc: './src/audio/coat.mp3',
+        },
+        {
+            word: 'shoe',
+            translation: 'туфли',
+            image: './src/img/shoe.jpg',
+            audioSrc: './src/audio/shoe.mp3',
+        },
+    ],
+    [
+        {
+            word: 'sad',
+            translation: 'грустный',
+            image: './src/img/sad.jpg',
+            audioSrc: './src/audio/sad.mp3',
+        },
+        {
+            word: 'angry',
+            translation: 'сердитый',
+            image: './src/img/angry.jpg',
+            audioSrc: './src/audio/angry.mp3',
+        },
+        {
+            word: 'happy',
+            translation: 'счастливый',
+            image: './src/img/happy.jpg',
+            audioSrc: './src/audio/happy.mp3',
+        },
+        {
+            word: 'tired',
+            translation: 'уставший',
+            image: './src/img/tired.jpg',
+            audioSrc: './src/audio/tired.mp3',
+        },
+        {
+            word: 'surprised',
+            translation: 'удивлённый',
+            image: './src/img/surprised.jpg',
+            audioSrc: './src/audio/surprised.mp3',
+        },
+        {
+            word: 'scared',
+            translation: 'испуганный',
+            image: './src/img/scared.jpg',
+            audioSrc: './src/audio/scared.mp3',
+        },
+        {
+            word: 'smile',
+            translation: 'улыбка',
+            image: './src/img/smile.jpg',
+            audioSrc: './src/audio/smile.mp3',
+        },
+        {
+            word: 'laugh',
+            translation: 'смех',
+            image: './src/img/laugh.jpg',
+            audioSrc: './src/audio/laugh.mp3',
+        },
+    ],
+];
 const hamburger = document.querySelector('.hamburger');
 const checkbox = document.getElementById('d');
-
+const mainMenu = document.querySelector('.categories-menu').innerHTML;
+let gameMode = false;
 document.addEventListener('click', (event) => {
-    if (event.target === hamburger) {
-       hamburger.classList.toggle('hamburger_active');
-       document.querySelector('.navigation').classList.toggle('active');
+    const targetElement = event.target;
+    const indexOfCategory = categories.indexOf(`${targetElement.dataset.id}`);
+    if (targetElement === hamburger) {
+        hamburger.classList.toggle('hamburger_active');
+        document.querySelector('.navigation').classList.toggle('active');
     }
-    if (event.target === checkbox) {
+    if (targetElement === checkbox) {
+        gameMode = !gameMode;
         document.querySelectorAll('.card-body').forEach((item) => {
             item.classList.toggle('green_color');
         });
         document.querySelector('.navigation').classList.toggle('green_color');
+        document.querySelectorAll('.flip-card-front').forEach((element) => {
+            element.children.item(1).classList.toggle('hidden');
+        });
+    }
+    if ((targetElement.classList.contains('mask') || targetElement.classList.contains('navigation__list_item')) && targetElement.innerText !== 'Main Menu') {
+        const elementOfCategory = document.querySelectorAll('.categories-menu__element');
+        elementOfCategory.forEach((elem) => {
+            while (elem.firstChild) {
+                elem.removeChild(elem.firstChild);
+            }
+        });
+        elementOfCategory.forEach((elem, index) => {
+            if (!gameMode) {
+                elem.innerHTML
+                    += `<div class="flip-card ">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <div class="view overlay">
+                                <audio data-sound="${[index]}" src="${cards[indexOfCategory][index].audioSrc}"></audio>
+                                <img class="card-img-top" data-id="card" src="${cards[indexOfCategory][index].image}" alt="Card image cap">
+                                    <a>          
+                                        <div class="rgba-white-slight"></div>
+                                    </a>
+                                </div>
+                                <div class="card-body black-color white-text rounded-bottom">
+                                    <a class="activator waves-effect mr-4"><i class="fas fa-share-alt white-text"></i></a>
+                                    <h4 class="card-title">${cards[indexOfCategory][index].word}</h4>
+                                    <img class="turn-card" src="./src/img/rotate.svg" data-id="swap" alt="swap">
+                                </div>
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="view overlay">
+                                    <img class="card-img-top" data-id="card" src="${cards[indexOfCategory][index].image}" alt="Card image cap">
+                                    <a>          
+                                        <div class="rgba-white-slight"></div>
+                                    </a>
+                                </div>
+                                <div class="card-body black-color white-text rounded-bottom">
+                                    <a class="activator waves-effect mr-4"><i class="fas fa-share-alt white-text"></i></a>
+                                    <h4 class="card-title">${cards[indexOfCategory][index].translation}</h4>
+                                </div>
+                            </div>
+                        </div>
+                   </div>`;
+            } else {
+            elem.innerHTML
+                += `<div class="flip-card ">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <div class="view overlay">
+                                <audio data-sound="${[index]}" src="${cards[indexOfCategory][index].audioSrc}"></audio>
+                                <img class="card-img-top" data-id="card" src="${cards[indexOfCategory][index].image}" alt="Card image cap">
+                                    <a>          
+                                        <div class="rgba-white-slight"></div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+            }
+        });
+    }
+    if (targetElement.innerText === 'Main Menu') {
+        if (!gameMode) {
+            document.querySelector('.categories-menu').innerHTML = mainMenu;
+        } else {
+            document.querySelector('.categories-menu').innerHTML = mainMenu;
+            document.querySelectorAll('.card-body').forEach((item) => {
+                item.classList.toggle('green_color');
+            });
+        }
+    }
+    if (targetElement.dataset.id === 'swap') {
+        const parent = targetElement.parentNode.parentNode.parentNode;
+        parent.classList.add('test');
+        document.addEventListener('mouseout', () => {
+            parent.classList.remove('test');
+        });
+    }
+    if (targetElement.dataset.id === 'card' && !gameMode) {
+        const audio = targetElement.parentNode.firstChild.nextSibling;
+        audio.play();
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,7 @@ module.exports = (env, options) => {
                 template: 'index.html',
             }),
             new MiniCssExtractPlugin({
+                template: './src/style.css',
                 filename: 'style.css',
             }),
         ],
